91816045727a7d9a7564c21467a6cb59417040a4
Shantanu <shantanu.suwarnkar@kcl.ac.uk>
2022-10-16 13:30:52 +0100

    fix csrf_token spelling

--- things/forms.py
+++ things/forms.py
@@ -7,4 +7,4 @@ from django import forms
 class ThingForm(forms.Form):
     name = forms.CharField(label='name', max_length=35)
     description = forms.CharField(widget=forms.Textarea, label='description')
-    quantity = forms.CharField(widget=forms.NumberInput(attrs={'min':0, 'max':50}), label='quantity')
+    quantity = forms.CharField(widget=forms.NumberInput(attrs={'min':1, 'max':50}), label='quantity')
