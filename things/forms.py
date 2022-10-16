dbcbe7e21a4318d7147669e08c512c39f1c6f983
Shantanu <shantanu.suwarnkar@kcl.ac.uk>
2022-10-16 13:36:31 +0100

    use model form

--- things/forms.py
+++ things/forms.py
@@ -1,10 +1,10 @@
 """Forms of the project."""
 
 # Create your forms here.
-
+from .models import Thing
 from django import forms
 
-class ThingForm(forms.Form):
-    name = forms.CharField(label='name', max_length=35)
-    description = forms.CharField(widget=forms.Textarea, label='description')
-    quantity = forms.CharField(widget=forms.NumberInput(attrs={'min':1, 'max':50}), label='quantity')
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
